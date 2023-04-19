ab8ba7b1facee6c41a0602826a41bdd3225f2b89
Rizqy H <rizqy@dicoding.com>
2023-04-19 22:18:10 +0700

    create base `EpisodeCard` component

---
+++ packages/app/src/components/EpisodeCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let episode: number;
+  export let title: string;
+  export let topic: string;
+  export let videoId: string;
+</script>
+
+<div class="flex mb-4">
+  <img
+    src="https://img.youtube.com/vi/{videoId}/mqdefault.jpg"
+    alt=""
+    class="w-1/3 rounded-md"
+  />
+  <div class="ml-4">
+    <span
+      class="px-2 py-1 rounded-md bg-orange-100 text-orange-600 text-sm font-medium"
+      >#{episode}</span
+    >
+    <h4 class="mt-2 mb-1">{title}</h4>
+    <span class="text-xs">Edisi: {topic}</span>
+  </div>
+</div>
---
+++ packages/app/src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+  import EpisodeCard from "../components/EpisodeCard.svelte";
+</script>
+
+<div class="p-4">
+  <EpisodeCard
+    episode={341}
+    title="Imam al-Ghazali"
+    topic="Divine Love (Mahabbatullah)"
+    videoId="oQiwYcrf35A"
+  />
+  <EpisodeCard
+    episode={340}
+    title="Ibnu Atha'illah al-Sakandari"
+    topic="Divine Love (Mahabbatullah)"
+    videoId="oV5VUovIoVw"
+  />
+  <EpisodeCard
+    episode={339}
+    title="Syaikh Abdul Qodir al-Jailani"
+    topic="Divine Love (Mahabbatullah)"
+    videoId="I4oo8KFR_8A"
+  />
+</div>
