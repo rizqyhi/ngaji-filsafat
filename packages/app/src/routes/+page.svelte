74b65daeda3113efdfad21176d5cd4d661a61889
Rizqy H <rizqy@dicoding.com>
2023-04-20 08:42:44 +0700

    load data from json file

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -1,24 +1,17 @@
-<script>
+<script lang="ts">
+  import type { PageData } from "./$types";
   import EpisodeCard from "../components/EpisodeCard.svelte";
+
+  export let data: PageData;
 </script>
 
 <div class="p-4">
-  <EpisodeCard
-    episode={341}
-    title="Imam al-Ghazali"
-    topic="Divine Love (Mahabbatullah)"
-    videoId="oQiwYcrf35A"
-  />
-  <EpisodeCard
-    episode={340}
-    title="Ibnu Atha'illah al-Sakandari"
-    topic="Divine Love (Mahabbatullah)"
-    videoId="oV5VUovIoVw"
-  />
-  <EpisodeCard
-    episode={339}
-    title="Syaikh Abdul Qodir al-Jailani"
-    topic="Divine Love (Mahabbatullah)"
-    videoId="I4oo8KFR_8A"
-  />
+  {#each data.episodes as episode (episode.id)}
+    <EpisodeCard
+      episode={episode.episode}
+      title={episode.title}
+      topic={episode.topic}
+      videoId={episode.video_ids[0]}
+    />
+  {/each}
 </div>
---
+++ packages/app/src/routes/+page.ts
@@ -0,0 +1,22 @@
+import type { PageLoad } from "./$types";
+
+type Episode = {
+  id: string;
+  episode: number;
+  date: string;
+  title: string;
+  topic: string;
+  is_official: boolean;
+  video_ids: string[];
+  video_url: string;
+  download_url: string;
+};
+
+export const load = (async ({ fetch }) => {
+  const response = await fetch("/database.json");
+  const episodes: Episode[] = await response.json();
+
+  return {
+    episodes,
+  };
+}) satisfies PageLoad;
