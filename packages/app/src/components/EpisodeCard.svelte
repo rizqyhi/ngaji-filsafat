acca7b0884baa5d2143376788d4503757512f1c6
Rizqy H <rizqy@dicoding.com>
2023-04-20 20:58:37 +0700

    adjust episode card style

--- packages/app/src/components/EpisodeCard.svelte
+++ packages/app/src/components/EpisodeCard.svelte
@@ -6,14 +6,16 @@
 </script>
 
 <div class="flex mb-4">
-  <img
-    src="https://img.youtube.com/vi/{videoId}/mqdefault.jpg"
-    alt=""
-    class="w-1/3 rounded-md"
-  />
+  <div class="w-1/3 flex-none">
+    <img
+      src="https://img.youtube.com/vi/{videoId}/mqdefault.jpg"
+      alt=""
+      class="rounded-md object-cover w-full aspect-video"
+    />
+  </div>
   <div class="ml-4">
     <span
-      class="px-2 py-1 rounded-md bg-orange-100 text-orange-600 text-sm font-medium"
+      class="px-2 py-1 rounded-md bg-orange-100 text-orange-600 text-xs font-medium tracking-wider"
       >#{episode}</span
     >
     <h4 class="mt-2 mb-1">{title}</h4>
