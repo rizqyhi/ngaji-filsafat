f1a1d51ecd91b7f80314ae433349acb0a2454a68
Rizqy H <rizqy@dicoding.com>
2023-04-22 19:56:03 +0700

    add back to home button on error page

--- packages/app/src/routes/+error.svelte
+++ packages/app/src/routes/+error.svelte
@@ -2,6 +2,8 @@
   import { page } from "$app/stores";
   import ContentWrapper from "../components/ContentWrapper.svelte";
   import Header from "../components/Header.svelte";
+  import { Icon } from "@steeze-ui/svelte-icon";
+  import { Home } from "@steeze-ui/heroicons";
 </script>
 
 <svelte:head>
@@ -11,7 +13,12 @@
 <Header />
 <ContentWrapper>
   <div class="py-16 text-center">
-    <h1 class="mb-8 text-6xl">:(</h1>
-    <p>{$page.status} {$page.error?.message}</p>
+    <h1 class="mb-6 text-6xl">:(</h1>
+    <div class="mb-16">{$page.status} {$page.error?.message}</div>
+
+    <a href="/" class="inline-flex items-center mx-auto">
+      <Icon src={Home} class="w-4 h-4 mr-2" />
+      Kembali ke beranda
+    </a>
   </div>
 </ContentWrapper>
