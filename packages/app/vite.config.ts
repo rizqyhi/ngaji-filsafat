f25bbbfc1caa3f16ceef89d4c6ce7796052844ba
Rizqy H <rizqy@dicoding.com>
2023-04-22 18:23:17 +0700

    fix icons package issue, see `steeze-ui/icons#15`

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [sveltekit()],
+  legacy: { buildSsrCjsExternalHeuristics: true },
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
   },
