d18180470512c54a8f7a54452388b01508a47b7a
Rizqy H <rizqy@dicoding.com>
2023-04-22 21:45:48 +0700

    redirect canonical link to slugged route

---
+++ packages/app/src/routes/[episode]/+page.server.ts
@@ -0,0 +1,27 @@
+import { error, redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+import { slugify } from "$lib/helpers";
+
+type Episode = {
+  id: string;
+  episode: number;
+  date: string;
+  title: string;
+  topic: string;
+  is_official: boolean;
+  video_ids: string[];
+  video_url: string;
+  download_url: string;
+};
+
+export const load = (async ({ fetch, params }) => {
+  const response = await fetch("/database.json");
+  const episodes: Episode[] = await response.json();
+  const episode = episodes.find((e) => e.episode.toString() === params.episode);
+
+  if (!episode) {
+    throw error(404, "Episode not found");
+  }
+
+  throw redirect(307, `/${episode.episode}/${slugify(episode.title)}`);
+}) satisfies PageServerLoad;
