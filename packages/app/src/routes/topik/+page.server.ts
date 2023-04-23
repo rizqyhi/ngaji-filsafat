61ea69870f4e93d0018347f4c65377265d18cd80
Rizqy H <rizqy@dicoding.com>
2023-04-23 17:52:46 +0700

    add episodes range on each topic list

--- packages/app/src/routes/topik/+page.server.ts
+++ packages/app/src/routes/topik/+page.server.ts
@@ -1,11 +1,11 @@
 import type { Episode } from "../../types";
 import type { PageServerLoad } from "./$types";
-import uniq from "lodash-es/uniq";
+import groupBy from "lodash-es/groupBy";
 
 export const load = (async ({ fetch }) => {
   const response = await fetch("/database.json");
   const episodes: Episode[] = await response.json();
-  const topics = uniq(episodes.map((episode) => episode.topic)).sort();
+  const topics = groupBy(episodes, "topic");
 
   return {
     topics,
--- packages/app/src/routes/topik/+page.svelte
+++ packages/app/src/routes/topik/+page.svelte
@@ -10,8 +10,17 @@
 
 <ContentWrapper withoutPadding>
   <ul>
-    {#each data.topics as topic}
-      <li class="p-4 border-b"><a href="/topik/{topic}">{topic}</a></li>
+    {#each Object.entries(data.topics) as [topic, episodes]}
+      <li class="p-4 border-b">
+        <a href="/topik/{topic}" class="flex items-center justify-between">
+          <span class="mr-4">{topic}</span>
+          <span
+            class="px-2 py-1 rounded-md bg-orange-100 text-orange-600 text-xs font-medium tracking-wider"
+            >#{episodes[0].episode} &ndash; #{episodes[episodes.length - 1]
+              .episode}</span
+          >
+        </a>
+      </li>
     {/each}
   </ul>
 </ContentWrapper>
