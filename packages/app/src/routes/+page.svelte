164d12daafa17b75d5c34a0f0e7f5639c3076778
Rizqy H <rizqy@dicoding.com>
2023-04-23 18:27:50 +0700

    add featured menu on homepage

--- packages/app/src/routes/+page.svelte
+++ packages/app/src/routes/+page.svelte
@@ -4,11 +4,26 @@
   import EpisodeCard from "../components/EpisodeCard.svelte";
   import Header from "../components/Header.svelte";
   import ContentWrapper from "../components/ContentWrapper.svelte";
+  import { Icon } from "@steeze-ui/svelte-icon";
+  import { RectangleStack, Hashtag } from "@steeze-ui/heroicons";
 
   export let data: PageData;
 </script>
 
 <Header title="Ngaji Filsafat" />
+
+<div
+  class="sticky top-0 grid grid-cols-2 gap-4 pt-4 pb-16 px-4 bg-gradient-to-r from-cyan-500 to-teal-400"
+>
+  <a href="/topik" class="p-4 bg-white rounded-lg shadow">
+    <Icon src={RectangleStack} class="w-6 h-6 mb-2" />
+    <span class="font-medium">Topik A-Z</span>
+  </a>
+  <a href="/topik" class="p-4 bg-white rounded-lg shadow">
+    <Icon src={Hashtag} class="w-6 h-6 mb-2" />
+    <span class="font-medium">Indeks Episode</span>
+  </a>
+</div>
 
 <ContentWrapper>
   {#each data.episodes as episode (episode.id)}
