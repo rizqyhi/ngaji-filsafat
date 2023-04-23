8657a9877284c4f0876f2d20c82e5b355de8e8c9
Rizqy H <rizqy@dicoding.com>
2023-04-23 08:32:06 +0700

    base handle for episode with multiple video parts

--- packages/app/src/routes/[episode]/[slug]/+page.svelte
+++ packages/app/src/routes/[episode]/[slug]/+page.svelte
@@ -5,6 +5,12 @@
 
   export let data: PageServerData;
   export const episode = data.episode;
+
+  let currentVideoIndex = 0;
+
+  function updateCurrentVideoIndex(index: number) {
+    currentVideoIndex = index;
+  }
 </script>
 
 <Header title="#{episode.episode}: {episode.title}" backTo="/" />
@@ -12,10 +18,20 @@
 <ContentWrapper>
   <iframe
     class="w-full aspect-video rounded-xl"
-    src="https://www.youtube-nocookie.com/embed/{episode.video_ids[0]}"
+    src="https://www.youtube-nocookie.com/embed/{episode.video_ids[
+      currentVideoIndex
+    ]}"
     title="YouTube video player"
     frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     allowfullscreen
   />
+
+  {#if episode.video_ids.length > 1}
+    {#each episode.video_ids as _, index}
+      <button class="mr-4" on:click={() => updateCurrentVideoIndex(index)}
+        >Bagian {index + 1}</button
+      >
+    {/each}
+  {/if}
 </ContentWrapper>
